ddfb2ca7d864e432b68b0f90bfdf867985e4ef05
BerkaySaygilarlii <127141599+BerkaySaygilarlii@users.noreply.github.com>
2024-12-10 18:38:32 +0300

    Fix port binding and add root route

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def get_transcript():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Heroku port
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port , debug=False)
